b568737e206acc1e05a7975bce38619b23ffbcff
Fatahul Ahmad Dzikri <fatahuldzikri@gmail.com>
2024-12-05 07:51:25 +0700

    mengubah carousel

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -2,7 +2,7 @@
 
 {{-- carousel --}}
 <div id="carouselExampleIndicators" class="carousel slide">
-    <div class="carousel-indicators">
+    {{-- <div class="carousel-indicators">
         <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
             aria-current="true" aria-label="Slide 1"></button>
         <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
@@ -11,12 +11,12 @@
             aria-label="Slide 3"></button>
         <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3"
             aria-label="Slide 4"></button>
-    </div>
+    </div> --}}
     <div class="carousel-inner">
         <div class="carousel-item active">
-            <img src="/img/carousel/1.png" class="d-block w-100" alt="...">
+            <img src="/img/carousel/elearning-01.png" class="d-block w-100" alt="...">
         </div>
-        <div class="carousel-item">
+        {{-- <div class="carousel-item">
             <img src="/img/carousel/carousel-02.png" class="d-block w-100" alt="...">
         </div>
         <div class="carousel-item">
@@ -24,9 +24,9 @@
         </div>
         <div class="carousel-item">
             <img src="/img/carousel/4.png" class="d-block w-100" alt="...">
-        </div>
+        </div> --}}
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+    {{-- <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
         data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Previous</span>
@@ -35,7 +35,7 @@
         data-bs-slide="next">
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Next</span>
-    </button>
+    </button> --}}
 </div>
 {{-- end carousel --}}
 
